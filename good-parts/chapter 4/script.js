a9eb290e4a9ccefdec4b283be4cb868ad838543c
akamornikov <akamornikov@qulix.com>
2015-01-20 17:29:41 +0300

    Finish Chapter 4

--- good-parts/chapter 4/script.js
+++ good-parts/chapter 4/script.js
@@ -1,4 +1,4 @@
-autowireTest();
+fibonacciTest();
 
 function applyTest() {
 	var Quo = function(str) {
@@ -42,7 +42,7 @@ function throwTest() {
 }
 
 function add(a, b) {
-	if (typeof a !== number || typeof b !== number) {
+	if (typeof a !== 'number' || typeof b !== 'number') {
 		throw {
 			name: 'TypeError',
 			message: 'add needs numbers'
@@ -56,4 +56,82 @@ function autowireTest() {
 	var str = "string";
 	console.log(str.test = 5);	//выведет 5
 	console.log(str.test);		//выведен undefined
+}
+
+function augmentingTest() {
+	String.prototype.repeat = function(times) {
+		var resultStr = "";
+		for (var i = 0; i < times; i++) {
+			resultStr += this;
+		}
+		return resultStr;
+	};
+	console.log("yo".repeat(4));
+}
+
+function trimTest() {
+	String.prototype.trim = function() {
+		return this.replace(/^\s+|\s+$/g, "");
+	};
+	console.log("    ADSAD sadasd     sdad     ".trim());
+}
+
+function factorialTest() {
+	var result = calculateFactorial(5);
+	console.log(result);	
+}
+
+function calculateFactorial(value) {
+	if (value === 1) {
+		return 1;
+	}
+	return value * calculateFibonacci(value - 1);
+}
+
+function protectedIncrementTest() {
+	var myObj = function() {
+		var value = 0;
+		return {
+			increment: function(inc) {
+				value += typeof inc === 'number' ? inc : 1;
+			},
+			getValue: function() {
+				return value;
+			}
+		}
+	}();
+
+	myObj.increment(20);
+	console.log(myObj.getValue());
+	myObj.increment(4);
+	console.log(myObj.getValue());
+}
+
+function protectedValueTest() {
+	var quo = function(value) {
+		return {
+			getValue: function() {
+				return value;
+			}
+		};
+	};
+
+	console.log(quo(5).getValue());
+}
+
+function fibonacciTest() {
+	var fibonacci = function(n) {
+		var memo = [0, 1];
+		var fib = function(n) {
+			var result = memo[n];
+			if (typeof result !== 'number') {
+				result = fib(n - 1) + fib(n - 2);
+				memo[n] = result;
+			}
+			return result;
+		};
+		return fib;
+	}();
+
+	console.log(fibonacci(10));
 }
